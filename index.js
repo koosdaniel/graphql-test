0670b296140ea73e02ac67f1d5402c8cda65ab35
Koos, Daniel <Daniel_Koos@epam.com>
2024-03-28 12:00:56 +0100

    feat: initialize Apollo server

---
+++ index.js
@@ -0,0 +1,21 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type Query {
+        totalDays: Int!
+    }
+`;
+
+const resolvers = {
+
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    // resolvers,
+    mocks: true
+});
+
+server.listen().then(({url}) => { 
+    console.log(`Server running at ${url}`);
+});
