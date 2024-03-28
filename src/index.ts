66c5d16e548f03e87c36d52d76735579e4299498
Koos, Daniel <Daniel_Koos@epam.com>
2024-03-28 12:26:55 +0100

    chore: install prettier

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,33 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
-    type ProgrammingDay {
-        id: ID!
-        date: String!
-        task: String!
-        conditions: Conditions
-    }
+  type ProgrammingDay {
+    id: ID!
+    date: String!
+    task: String!
+    conditions: Conditions
+  }
 
-    enum Conditions {
-        BUSY
-        NORMAL
-        BORING
-    }
+  enum Conditions {
+    BUSY
+    NORMAL
+    BORING
+  }
 
-    type Query {
-        totalDays: Int!,
-        allDays: [ProgrammingDay!]!
-    }
+  type Query {
+    totalDays: Int!
+    allDays: [ProgrammingDay!]!
+  }
 `;
 
-const resolvers = {
-
-};
+const resolvers = {};
 
 const server = new ApolloServer({
-    typeDefs,
-    // resolvers,
-    mocks: true
+  typeDefs,
+  // resolvers,
+  mocks: true,
 });
 
-server.listen().then(({url}: {url: string}) => { 
-    console.log(`Server running at ${url}`);
+server.listen().then(({ url }: { url: string }) => {
+  console.log(`Server running at ${url}`);
 });
