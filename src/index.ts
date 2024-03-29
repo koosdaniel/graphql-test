3e16ec7a5d2b7258f3eef296865760437fa354a2
Koos, Daniel <Daniel_Koos@epam.com>
2024-03-29 13:18:53 +0100

    chore: add prettierrc and apply config

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,54 @@
-import { ApolloServer, gql } from "apollo-server";
-import casual from "casual";
+import { ApolloServer, gql } from 'apollo-server';
+import casual from 'casual';
 
 const typeDefs = gql`
-  scalar Date
+    scalar Date
 
-  """
-  An object that describes the characteristics of a programming day
-  """
-  type ProgrammingDay {
-    "A programming day unique identifier"
-    id: ID!
-    "The date that the day occured"
-    date: Date!
-    "The task that was completed on a day"
-    task: String!
-    conditions: Conditions
-  }
+    """
+    An object that describes the characteristics of a programming day
+    """
+    type ProgrammingDay {
+        "A programming day unique identifier"
+        id: ID!
+        "The date that the day occured"
+        date: Date!
+        "The task that was completed on a day"
+        task: String!
+        conditions: Conditions
+    }
 
-  enum Conditions {
-    BUSY
-    NORMAL
-    BORING
-  }
+    enum Conditions {
+        BUSY
+        NORMAL
+        BORING
+    }
 
-  type Query {
-    totalDays: Int!
-    allDays: [ProgrammingDay!]!
-  }
+    type Query {
+        totalDays: Int!
+        allDays: [ProgrammingDay!]!
+    }
 
-  input AddDayInput {
-    date: Date!
-    task: String!
-    conditions: Conditions
-  }
+    input AddDayInput {
+        date: Date!
+        task: String!
+        conditions: Conditions
+    }
 
-  type RemoveDayPayload {
-    day: ProgrammingDay!
-    removed: Boolean
-    totalBefore: Int
-    totalAfter: Int
-  }
+    type RemoveDayPayload {
+        day: ProgrammingDay!
+        removed: Boolean
+        totalBefore: Int
+        totalAfter: Int
+    }
 
-  type Mutation {
-    addDay(input: AddDayInput!): ProgrammingDay!
-    removeDay(id: ID!): RemoveDayPayload!
-  }
+    type Mutation {
+        addDay(input: AddDayInput!): ProgrammingDay!
+        removeDay(id: ID!): RemoveDayPayload!
+    }
 
-  type Subscription {
-    newDay: ProgrammingDay!
-  }
+    type Subscription {
+        newDay: ProgrammingDay!
+    }
 `;
 
 /*
@@ -76,21 +76,21 @@ mutation {
 // const resolvers = {};
 
 const mocks = {
-  Date: () => "1/2/2024",
-  String: () => "Cool Data",
-  Query: () => ({
-    allDays: () => [...new Array(casual.integer(1, 5))],
-    // Works in Apollo v2 only
-    // allDays: () => new MockList([1, 15])
-  }),
+    Date: () => '1/2/2024',
+    String: () => 'Cool Data',
+    Query: () => ({
+        allDays: () => [...new Array(casual.integer(1, 5))]
+        // Works in Apollo v2 only
+        // allDays: () => new MockList([1, 15])
+    })
 };
 
 const server = new ApolloServer({
-  typeDefs,
-  // resolvers,
-  mocks,
+    typeDefs,
+    // resolvers,
+    mocks
 });
 
 server.listen().then(({ url }) => {
-  console.log(`Server running at ${url}`);
+    console.log(`Server running at ${url}`);
 });
